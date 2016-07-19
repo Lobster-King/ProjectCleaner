c851c27764ae8ff3d04404f61fe762ab86cc0348
秦志伟 <win_cooperation@163.com>
2016-07-19 09:54:32 +0800

    edit install sh
    edit install sh

--- ProjectCleaner/install.sh
+++ ProjectCleaner/install.sh
@@ -10,7 +10,6 @@ PLIST_PLUGINS_KEY="DVTPlugInManagerNonApplePlugIns-Xcode-${XCODE_VERSION}"
 BUNDLE_ID="com.lobster.ProjectCleaner"
 TMP_FILE="$(mktemp -t ${BUNDLE_ID})"
 
-
 if defaults read com.apple.dt.Xcode "$PLIST_PLUGINS_KEY" &> "$TMP_FILE"; then
 
 /usr/libexec/PlistBuddy -c "delete skipped:$BUNDLE_ID" "$TMP_FILE" > /dev/null 2>&1 && {
@@ -20,21 +19,18 @@ echo 'An instance of Xcode is currently running.' \
 exit 1
 }
 defaults write com.apple.dt.Xcode "$PLIST_PLUGINS_KEY" "$(cat "$TMP_FILE")"
-echo 'Alcatraz was removed from Xcode'\''s skipped plugins list.' \
+echo 'ProjectCleaner was removed from Xcode'\''s skipped plugins list.' \
 'Next time you start Xcode select "Load Bundle" when prompted.'
 }
 else
 
 KNOWN_WARNING="The domain/default pair of \(.+, $PLIST_PLUGINS_KEY\) does not exist"
 
-# tr: For some mysterious reason, some `defaults` errors are outputed on two lines.
-# grep: -v returns 1 when output is empty (ie. we filtered the known warning)
-# so we exit on 0, which means an unknown error occured.
 tr -d '\n' < "$TMP_FILE" | egrep -v "$KNOWN_WARNING" && exit 1
 fi
 rm -f "$TMP_FILE"
 
-# Download and install Alcatraz
+# Download and install ProjectCleaner
 mkdir -p "${PLUGINS_DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
 
